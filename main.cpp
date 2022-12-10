a7af41601da47c885cf877409c62ab22f4f5007f
Volodya Ismailyan <nenie_iri@mail.ru>
2022-12-10 12:33:18 +0400

    copy my tests from 42_Ft_containers repo

---
+++ main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/22 12:20:25 by vismaily          #+#    #+#             */
+/*   Updated: 2022/10/31 16:16:26 by vismaily         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Containers/vector.hpp"
+#include "Containers/stack.hpp"
+#include "Containers/map.hpp"
+#include <vector>
+#include <stack>
+#include <map>
+#include <iostream>
+#include <string>
+
+void	vector_test();
+void	stack_test();
+void	map_test();
+
+int	main()
+{
+	vector_test();
+	stack_test();
+	map_test();
+}
