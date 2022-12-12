fbd706e755029d985a60bf3e0eb3da25787ba961
Volodya Ismailyan <nenie_iri@mail.ru>
2022-12-12 19:50:17 +0400

    trying another way

--- start.sh
+++ start.sh
@@ -2,7 +2,15 @@
 
 . ./config.sh
 
-make fclean
-make Vdef=$vector Sdef=$stack Mdef=$map
+#make fclean
+#make Vdef=$vector Sdef=$stack Mdef=$map
+#./crash_test
 
-./crash_test
+flags="-Wall -Wextra -Werror -I./";
+defines="-D NS=ft -D NS_TEST=ft_test -D V=\"$vector\" -D S=\"$stack\" -D M=\"$map\"";
+tmp="Tmp";
+
+mkdir -p $tmp;
+c++ $flags $defines -c ./Srcs/Vector/vec_example1.cpp -o ./Tmp/vec_exmple1_ft.o;
+
+rm -rf $tmp;
