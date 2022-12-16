1bfe413545054c762fc4d72c7622eec533240387
Volodya Ismailyan <nenie_iri@mail.ru>
2022-12-16 12:56:37 +0400

    add time counter

--- Srcs/Vector/vec_example28.cpp
+++ Srcs/Vector/vec_example28.cpp
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 13:29:35 by vismaily          #+#    #+#             */
-/*   Updated: 2022/12/15 19:27:03 by tumolabs         ###   ########.fr       */
+/*   Updated: 2022/12/16 12:34:42 by tumolabs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ namespace	NS_TEST
 		gettimeofday(&end, NULL);
 
 		_time = (end.tv_sec - start.tv_sec) * 1e6;
-		_time = (_time + (end.tv_usec - start.tv_usec)) * 1e-6;
+		_time = (_time + (end.tv_usec - start.tv_usec));
 
 		ss << " " << _vector[444];
 		ss << " " << _vector.size();
--- Srcs/run.cpp
+++ Srcs/run.cpp
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 12:20:25 by vismaily          #+#    #+#             */
-/*   Updated: 2022/12/15 19:19:18 by tumolabs         ###   ########.fr       */
+/*   Updated: 2022/12/16 12:55:31 by tumolabs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,51 @@ void	run(int num, const std::string (*f1)(double &), const std::string (*f2)(dou
 	std::string			str1;
 	std::string			str2;
 	char				diff;
-	double				_time;
+	double				time1;
+	double				time2;
 
-	_time = 0;
-	str1 = (*f1)(_time);
-	std::cout << std::endl << _time << std::endl;
-	str2 = (*f2)(_time);
-	std::cout << std::endl << _time << std::endl;
+	time1 = 0;
+	time2 = 0;
+	std::cout << COLOR_YELLOW_B << "TIME: " << std::flush;
+	str1 = (*f1)(time1);
+	str2 = (*f2)(time2);
+	time2 /= 20;
 
-	std::cout << COLOR_YELLOW_B << "TEST: ";
+	if ((time2 < time1) || (time1 == 0))
+	{
+		std::cout << COLOR_GREEN_B << "✅ OK " << std::flush;
+   		std::cout << COLOR_PURPLE_B << "| " << COLOR_END << std::flush;
+	}
+	else
+	{
+		std::cout << COLOR_RED_B << "❌ KO (TIMEOUT)" << std::endl << COLOR_END;
+		std::cout << COLOR_RED_B << "Your grade is 0/100. Fix mistake and try again!!!";
+		std::cout << std::endl << COLOR_END;
+		do
+		{
+			std::cout << COLOR_YELLOW_B;
+			std::cout << "Do you want to see time difference? [y] or [n]: ";
+			std::cout << COLOR_END << std::flush;
+			std::cout << COLOR_PURPLE_B;
+			std::cin >> diff;
+			std::cout << COLOR_END;
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			if (std::cin.eof() || std::cin.fail())
+				break ;
+		} while (diff != 'y' && diff != 'n');
+		if (diff == 'y')
+		{
+			std::cout << std::setprecision(2);
+			std::cout << COLOR_GREEN_B << "std -> " << std::left << std::setw(4);
+			std::cout << (time1 * 1e-6) << std::endl;
+			std::cout << COLOR_RED_B << "ft  -> ";
+			std::cout << std::left << std::fixed << std::setw(4);
+			std::cout << (time2 * 20 * 1e-6) << COLOR_END << std::endl;
+		}
+		ss << num;
+		exit(1);
+	}
+	std::cout << COLOR_YELLOW_B << "TEST: " << std::flush;
 	if (str1 == str2)
 		std::cout << COLOR_GREEN_B << "✅ OK" << COLOR_END;
 	else
@@ -37,7 +73,8 @@ void	run(int num, const std::string (*f1)(double &), const std::string (*f2)(dou
 		do
 		{
 			std::cout << COLOR_YELLOW_B;
-			std::cout << "Do you want to see differents? [y] or [n]: " << COLOR_END;
+			std::cout << "Do you want to see results difference? [y] or [n]: ";
+			std::cout << COLOR_END << std::flush;
 			std::cout << COLOR_PURPLE_B;
 			std::cin >> diff;
 			std::cout << COLOR_END;
